d44edef0a4e2f8b8c516bcf78d8e5940185972b0
Michele Ceriotti <michele.ceriotti@gmail.com>
2021-11-05 11:03:49 +0100

    A better (more sklearn-compatible) multi-model implementation

--- skcosmo/linear_model/_atom_ridge.py
+++ skcosmo/linear_model/_atom_ridge.py
@@ -2,47 +2,55 @@ import numpy as np
 from sklearn.base import (
     MultiOutputMixin,
     RegressorMixin,
+    BaseEstimator
 )
 from sklearn.linear_model import Ridge
 
-class AtomRidge(MultiOutputMixin, RegressorMixin):
+class AtomRidge(BaseEstimator, MultiOutputMixin, RegressorMixin):
     """ Solves a regularized linear regression problem for a model meant
     to describe a set of atom-centered properties. Difference with conventional
     Ridge involves mainly the definition of regularization, that is structured
     as an "error tolerance". This also supports computing combinations of
     multiple models, that are fit jointly. """
 
-    def __init__(self, sigma, delta, *, solver='auto'):
-        if not hasattr(delta, "__len__"):
-            delta = [delta]
+    def __init__(self, delta, sigma, *, feature_groups=None, solver='auto'):
         self.delta = delta
         self.sigma = sigma
         self.solver = solver
+        self.feature_groups = feature_groups
 
-    def fit(self, X, y):
-        if not hasattr(X, "__len__"):
-            X = [X]
+    def fit(self, X, y, sample_sigmas=None):
+
+        # parameter validation
+        if not hasattr(self.delta, "__len__"):
+            self.delta = [self.delta]
+        if self.feature_groups is None:
+           feature_groups.feature_groups = [slice(None)]
+        if len(self.feature_groups) != len(self.delta):
+            raise ValueError(f"Number of models  {len(self.feature_groups)} doesn't match number of delta parameters {len(self.delta)}.")
 
         # enforces multi-output shape for y
         moy = y
         if len(y.shape)==1:
             moy = y.reshape((-1,1))
 
-        if not(len(X) == len(self.delta)):
-            raise ValueError(f"Number of model features  {len(X)} doesn't match number of delta parameters {len(self.delta)}.")
+        if self.feature_groups is None:
+            self.feature_groups = [slice(None)]
         normalization = []
-        for x in X:
-            normalization.append(np.sqrt((x**2).sum(axis=1).mean()))
+        for g in self.feature_groups:
+            normalization.append(np.sqrt((X[:,g]**2).sum(axis=1).mean()))
         self.normalization_ = np.asarray(normalization)
 
-        X = np.hstack([X[i]*self.delta[i]/normalization[i] for i in range(len(X))])
+        nX = np.hstack([X[:,g]*self.delta[ig]/self.normalization_[ig] for ig, g in enumerate(self.feature_groups)])
 
         # scales by sigma - this works whether sigma is a scalar or a Ntrain-sized array
-        X = (X.T / np.sqrt(self.sigma)).T
-        moy = (moy.T / np.sqrt(self.sigma)).T
+        if sample_sigmas is None:
+            sample_sigmas = self.sigma
+        nX = (nX.T / np.sqrt(sample_sigmas)).T
+        moy = (moy.T / np.sqrt(sample_sigmas)).T
 
         ridge = Ridge(alpha=1, fit_intercept=False, solver=self.solver)
-        ridge.fit(X, moy)
+        ridge.fit(nX, moy)
         self.coef_ = ridge.coef_
 
         # restores whatever the user expects in terms of output shape
@@ -51,10 +59,8 @@ class AtomRidge(MultiOutputMixin, RegressorMixin):
 
     def predict(self, X):
 
-        if not hasattr(X, "__len__"):
-            X = [X]
-        X = np.hstack([X[i]*self.delta[i]/self.normalization_[i] for i in range(len(X))])
-        return X@self.coef_
+        nX = np.hstack([X[:,g]*self.delta[ig]/self.normalization_[ig] for ig, g in enumerate(self.feature_groups)])
+        return nX@self.coef_
 
 
 
